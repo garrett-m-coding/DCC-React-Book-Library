e48635bcc2923311dec853514ed2e553e47f8feb
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-10-06 14:01:19 -0500

    Split book viewing login into its own component file bookviewer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css'
 import TitleBar from './TitleBar/TitleBar';
+import BookViewer from './BookViewer/BookViewer';
 
 
 class App extends Component {
@@ -21,8 +22,19 @@ class App extends Component {
     goToNextBook = () => {
         let tempBookNumber = this.state.bookNumber;
         tempBookNumber++;
-        if(tempBookNumber == this.books.length){
+        if (tempBookNumber == this.books.length) {
             tempBookNumber = 0;
+        }
+        this.setState({
+            bookNumber: tempBookNumber
+        });
+    }
+
+    goToPreviousBook  = () => {
+        let tempBookNumber = this.state.bookNumber;
+        tempBookNumber--;
+        if (tempBookNumber < 0) {
+            tempBookNumber = this.books.length - 1;
         }
         this.setState({
             bookNumber: tempBookNumber
@@ -33,20 +45,7 @@ class App extends Component {
         return (
             <div className="container-fluid">
                 <TitleBar />
-                <div className="row">
-                    <div className="col-md-4">
-                        {/*Button here to move to the previous book viewed*/}
-                    </div>
-                    <div className="col-md-4">
-                        {/*Display book with cover here*/}
-                        <h1>{this.books[this.state.bookNumber].title}</h1>
-                        <h4>{this.books[this.state.bookNumber].author}</h4>
-                    </div>
-                    <div className="col-md-4">
-                        {/*Button here to move to the next book viewed*/}
-                        <button onClick={this.goToNextBook}>Next Book</button>
-                    </div>    
-                </div>    
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={this.goToNextBook} previousBook={this.goToPreviousBook} />
             </div>
         )
     }
